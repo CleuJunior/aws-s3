184c2615f5d2f8619f6972455b28829f301c038c
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2024-03-21 23:31:27 -0300

    Integration Test and flyway

--- backend/src/main/java/backendapi/dto/CustomerResponse.java
+++ backend/src/main/java/backendapi/dto/CustomerResponse.java
@@ -1,12 +1,14 @@
 package backendapi.dto;
 
 import backendapi.model.Gender;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
 public record CustomerResponse(Integer id,
                                String name,
                                String email,
+                               @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
                                LocalDateTime birthdate,
                                Gender gender,
                                String password,
--- backend/src/test/java/backendapi/integration/controller/CustomerControllerIT.java
+++ backend/src/test/java/backendapi/integration/controller/CustomerControllerIT.java
@@ -1,26 +1,48 @@
 package backendapi.integration.controller;
 
-import backendapi.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static backendapi.integration.controller.CustomerJsonUtils.customerJson;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc
 class CustomerControllerIT extends IntegrationTest {
 
     @Autowired
-    private CustomerRepository repository;
-//    @Autowired
-//    private MockMvc mockMvc;
-
-//    @BeforeEach
-//    void setUp() {
-//        repository.saveAll(createCustomers());
-//    }
+    private MockMvc mockMvc;
+    private static final String ENDPOINT = "/api/v1/customers";
 
     @Test
-    void testtest() {
-        assertThat(postgres.isCreated()).isTrue();
+    void shouldReturnListCustomer() throws Exception {
+        mockMvc
+                .perform(get(ENDPOINT))
+                .andDo(print())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(content().json(customerJson()));
     }
 
+    @Test
+    void shouldReturnPageCustomer() throws Exception {
+        var json = "{\"content\":[{\"id\":1,\"name\":\"Whatz\",\"email\":\"whatz.junior@gmail.com\",\"birthdate\":\"1990-11-09 22:13:00\",\"gender\":\"FEMALE\",\"password\":\"123456789\",\"profileImageId\":\"image1\"}],\"pageable\":{\"pageNumber\":0,\"pageSize\":1,\"sort\":{\"empty\":true,\"sorted\":false,\"unsorted\":true},\"offset\":0,\"paged\":true,\"unpaged\":false},\"last\":false,\"totalElements\":25,\"totalPages\":25,\"size\":1,\"number\":0,\"sort\":{\"empty\":true,\"sorted\":false,\"unsorted\":true},\"numberOfElements\":1,\"first\":true,\"empty\":false}";
+
+        mockMvc
+                .perform(get(ENDPOINT)
+                        .queryParam("page", "0")
+                        .queryParam("size", "1")
+                )
+                .andDo(print())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(json));
+    }
 }
